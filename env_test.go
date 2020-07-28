bed39266a6b25ca0a3e37fd1b9189a6e965eb3db
sherylm77 <sherylmathew77@gmail.com>
2020-07-28 16:01:13 -0700

    File to test angle calculations

---
+++ env_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"math"
+	"math/rand"
+	"testing"
+)
+
+func TestAngle(t *testing.T) {
+	for i := 0; i < 50; i++ {
+		Point := image.Point{}
+		Point2 := image.Point{}
+		Point.X = 4
+		Point.Y = 4
+		Size := 10
+		for {
+			Point2.X = rand.Intn(Size)
+			Point2.Y = rand.Intn(Size)
+			if Point2 != Point {
+				break
+			}
+		}
+		xDist := Point2.X - Point.X
+		yDist := Point2.Y - Point.Y
+		ang0 := 0.0
+		ang360 := 0.0
+		if xDist >= 0 && yDist >= 0 {
+			ang0 = math.Atan2(float64(yDist), float64(xDist)) * 180 / math.Pi
+		} else if xDist < 0 && yDist > 0 {
+			ang0 = 180 + (math.Atan2(float64(yDist), float64(xDist)) * 180 / math.Pi)
+		} else if xDist >= 0 && yDist < 0 {
+			ang360 = math.Abs(math.Atan2(float64(yDist), float64(xDist))) * 180 / math.Pi
+		} else { //xDist < 0 and yDist < 0
+			ang360 = 180 - (math.Atan2(float64(yDist), float64(xDist)) * 180 / math.Pi)
+		}
+		ang := ang0 + ang360
+		fmt.Printf("%v %v %v %v \n", Point2, ang, ang0, ang360)
+	}
+
+}
